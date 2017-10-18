39dc99123157802ab12db63fd330579876b78818
Arialdo Martini <arialdomartini@gmail.com>
2017-10-18 11:57:36 +0200

    No more old scripts for docker compose and swarm

--- run.bat
+++
@@ -1,6 +0,0 @@
-docker network create  --driver=overlay mynet
-
-docker service create --name=web      --replicas=1 --network=mynet --publish 9000:9000                       adunicorn/issuing
-docker service create --name=rabbit   --replicas=1 --network=mynet --publish 5672:5672 --publish 15672:15672 rabbitmq:3-management
-docker service create --name=redis    --replicas=1 --network=mynet --publish=6379:6379                       redis
-docker service create --name=consumer --replicas=1 --network=mynet                                           adunicorn/rabbitconsumer
