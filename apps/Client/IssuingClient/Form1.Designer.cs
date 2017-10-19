02dc2e495e45d27a6f719b3f538e914cd5232166
Firmani Emanuele <FIRE0272@adgr.net>
2017-10-19 14:41:49 +0200

    IssuingClient generates boxes with changing colors

--- apps/Client/IssuingClient/Form1.Designer.cs
+++ apps/Client/IssuingClient/Form1.Designer.cs
@@ -28,25 +28,50 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.box = new System.Windows.Forms.GroupBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // box
+            // 
+            this.box.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.box.Location = new System.Drawing.Point(12, 58);
+            this.box.Name = "box";
+            this.box.Size = new System.Drawing.Size(811, 425);
+            this.box.TabIndex = 0;
+            this.box.TabStop = false;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(185, 123);
+            this.button1.Location = new System.Drawing.Point(659, 12);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Start";
             this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(748, 13);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Stop";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.ClientSize = new System.Drawing.Size(835, 495);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
+            this.Controls.Add(this.box);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -55,7 +80,9 @@
 
         #endregion
 
+        private System.Windows.Forms.GroupBox box;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- apps/Client/IssuingClient/Form1.cs
+++ apps/Client/IssuingClient/Form1.cs
@@ -1,25 +1,85 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace IssuingClient
 {
     public partial class Form1 : Form
     {
+        delegate void CallerDelegate(Panel panel);
+        readonly Random _rnd = new Random();
+        private static bool _stop;
+
         public Form1()
         {
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void GenerateCallers(int count, string target)
         {
-            MessageBox.Show("Ciao");
+            box.Controls.Clear();
+
+            var panels = new List<Panel>();
+            int top = 13;
+            int colIndex = 0;
+
+            for (int i = 0; i < count; i++)
+            {
+                int squareSize = 50;
+
+                var p = new Panel();
+                p.Top = top;
+                p.Left = 5 + (squareSize + 5) * colIndex;
+                p.Height = squareSize;
+                p.Width = squareSize;
+
+                p.BackColor = Color.White;
+
+                box.Controls.Add(p);
+
+                panels.Add(p);
+                colIndex++;
+                if (colIndex % 10 == 0)
+                {
+                    colIndex = 0;
+                    top += 55;
+                }
+            }
+
+            foreach (var p in panels)
+            {
+                var caller = new CallerDelegate(StartCaller);
+                caller.BeginInvoke(p, null, null);
+            }
+        }
+
+        private void StartCaller(Panel panel)
+        {
+            while (!_stop)
+            {
+                WaitRand(500);
+                panel.BackColor = Color.AntiqueWhite;
+
+                WaitRand(500);
+                panel.BackColor = Color.BlueViolet;
+            }
+        }
+
+        private void WaitRand(int ms)
+        {
+                Thread.Sleep(_rnd.Next(ms));
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            GenerateCallers(32, "");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            _stop = true;
         }
     }
 }
