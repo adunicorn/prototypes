4779abd34520eddc8d3c04e378de69098db9220d
Arialdo Martini <arialdomartini@gmail.com>
2017-10-19 10:00:11 +0200

    scripts for compiling, building and publishing

--- apps/Loader/build-and-push.sh
+++ apps/Loader/build-and-push.sh
@@ -1,4 +1,3 @@
-source ../../docker-login.sh
 reg=$(minishift openshift registry)
 
 docker build -t $reg/issuing/loader .
--- build-and-push-all.sh
+++ build-and-push-all.sh
@@ -1,5 +1,17 @@
 set -e
 
-(cd apps && ./build-and-push-all.sh)
+source minishift-docker-login.sh
+
+echo -e "\n\nBuilding RabbitMQ Consumer image..."
+(cd apps/IssuingCluster/RabbitConsumer && ./build-and-push.sh)
+
+echo -e "\n\nBuilding the IssuingService image..."
+(cd apps/IssuingCluster/IssuingService && ./build-and-push.sh)
+
+echo -e "\n\nBuilding the Loader image..."
+(cd apps/Loader && ./build-and-push.sh)
+
+echo -e "\n\nBuilding the OldIssuingService image..."
+(cd apps/IssuingCluster/OldIssuingService && ./build-and-push.sh)
 
 echo Done
