bca4bb682ed31b5f118f7a64ef2b20eda7334a51
Arialdo Martini <arialdomartini@gmail.com>
2017-11-14 09:52:58 +0100

    Issuing service dies more gracefully (using an async while cycle)

--- apps/IssuingCluster/IssuingService/Program.cs
+++ apps/IssuingCluster/IssuingService/Program.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using System.Threading;
 using Microsoft.Owin.Hosting;
+using System.Threading.Tasks;
 
 namespace IssuingService
 {
     public class Program
     {
-        public static string Version = "v33 new";
+        public static string Version = "v35 new";
         public static string RedisMasterHostName = Environment.GetEnvironmentVariable("REDIS_MASTER_SERVICE_HOST");
         public static string RedisSlaveHostName = "localhost";
         public static string RedisPassword = "redis";
@@ -21,7 +22,13 @@ namespace IssuingService
             {
                 Console.WriteLine("App started with address '{0}' on a machine '{1}' with OS '{2}'", baseAddress, Environment.MachineName, Environment.OSVersion);
                 Console.WriteLine();
-                Thread.Sleep(Timeout.Infinite);
+                while(true)
+                {
+                    Task.Run(async () =>
+                    {
+                        await Task.Delay(500);
+                    }).GetAwaiter().GetResult();
+                }
             }
         }
     }
