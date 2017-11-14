e2e3022344198ec34b28abe886bdacd85ceaf0cc
Arialdo Martini <arialdomartini@gmail.com>
2017-11-14 07:37:56 +0100

    Less replicas

--- apps/Loader/app/create.sh
+++ apps/Loader/app/create.sh
@@ -1,10 +1,13 @@
 echo "Creating schema..."
 python /app/execsql.py $POSTGRESQL_SERVICE_HOST /app/sql/schema.sql
+echo "Schema created!"
 
 echo "Filling table..."
 python /app/execsql.py $POSTGRESQL_SERVICE_HOST /app/sql/values.sql
+echo "Table filled!"
 
-echo "FIlling Redis..."
+echo "Filling Redis..."
 python /app/load-into-redis.py
+echo "Redis filled!"
 
 tail -f /dev/null
