39211ac1fc5d93f9ace9386f17c58fd9ae488a17
Arialdo Martini <arialdomartini@gmail.com>
2017-11-14 06:31:23 +0100

    Loader image fills up Redis at startup

--- apps/Loader/app/create.sh
+++ apps/Loader/app/create.sh
@@ -3,4 +3,8 @@ python /app/execsql.py $POSTGRESQL_SERVICE_HOST /app/sql/schema.sql
 
 echo "Filling table..."
 python /app/execsql.py $POSTGRESQL_SERVICE_HOST /app/sql/values.sql
+
+echo "FIlling Redis..."
+python /app/load-into-redis.py
+
 tail -f /dev/null
